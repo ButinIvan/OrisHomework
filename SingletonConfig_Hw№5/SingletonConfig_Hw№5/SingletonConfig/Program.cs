97e53cbc8fa65e8dfcb17b8413931558070617c3
Ivan <butin.vanechka@gmail.com>
2025-02-09 18:07:15 +0300

    Added 5th hw

---
+++ SingletonConfig_Hw№5/SingletonConfig_Hw№5/SingletonConfig/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SingletonConfig;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var config = AppConfig.Instance;
+        Console.WriteLine(config.Domain);
+        Console.WriteLine(config.Port);
+        Console.WriteLine(config.StaticDirectoryPath);
+    }
+}
